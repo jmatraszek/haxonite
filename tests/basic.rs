da3a8211221dcb48c478baaae874a6a4f5e92753
Jakub Matraszek <jakub.matraszek@gmail.com>
2018-04-30 23:09:53 +0200

    Implement one more test

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,10 +1,9 @@
-mod common;
-// use common::*;
-use common::common::*;
+mod test_helper;
+use test_helper::*;
 
 #[test]
 fn create_basic_project_test() {
-    create_basic_project("basic").expect("Creating project failed.");
+    create_basic_project("basic");
 
     assert_file_exists("basic");
     assert_file_exists("basic/config.toml");
@@ -14,6 +13,26 @@ fn create_basic_project_test() {
     assert_file_contents("basic/config.toml");
     assert_file_contents("basic/responses/haxonite.json");
 
-    remove_test_project("basic")
+    remove_test_project("basic");
+}
+
+#[test]
+fn test_basic_project_test() {
+    create_basic_project("basic2");
+
+    start_project("basic2", || {
+        get_request("/", |mut response| {
+            assert_status(&response, StatusCode::Ok);
+            assert_header(&response, "Content-Type", "application/json");
+        });
+
+        get_request("/public/haxonite.png", |response| {
+            assert_status(&response, StatusCode::Ok);
+            assert_header(&response, "Content-Type", "image/png");
+            assert_header(&response, "Content-Length", "60629");
+        });
+    });
+
+    remove_test_project("basic2");
 }
 
