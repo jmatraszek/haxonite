99715491435486230d4d593eb219a1d224de96b7
Jakub Matraszek <jakub.matraszek@gmail.com>
2018-05-01 23:36:57 +0200

    Rewrite asserts as macros

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,17 +1,17 @@
-mod test_helper;
+#[macro_use] mod test_helper;
 use test_helper::*;
 
 #[test]
 fn create_basic_project_test() {
     create_basic_project("basic");
 
-    assert_file_exists("basic");
-    assert_file_exists("basic/config.toml");
-    assert_file_exists("basic/responses/haxonite.json");
-    assert_file_exists("basic/assets/haxonite.png");
+    assert_file_exists!("basic");
+    assert_file_exists!("basic/config.toml");
+    assert_file_exists!("basic/responses/haxonite.json");
+    assert_file_exists!("basic/assets/haxonite.png");
 
-    assert_file_contents("basic/config.toml");
-    assert_file_contents("basic/responses/haxonite.json");
+    assert_file_contents!("basic/config.toml");
+    assert_file_contents!("basic/responses/haxonite.json");
 
     remove_test_project("basic");
 }
@@ -21,15 +21,15 @@ fn test_basic_project_test() {
     create_basic_project("basic2");
 
     start_project("basic2", || {
-        get_request("/", |mut response| {
-            assert_status(&response, StatusCode::Ok);
-            assert_header(&response, "Content-Type", "application/json");
+        get_request("/", |response| {
+            assert_status!(&response, StatusCode::Ok);
+            assert_header!(&response, "Content-Type", "application/json");
         });
 
         get_request("/public/haxonite.png", |response| {
-            assert_status(&response, StatusCode::Ok);
-            assert_header(&response, "Content-Type", "image/png");
-            assert_header(&response, "Content-Length", "60629");
+            assert_status!(&response, StatusCode::Ok);
+            assert_header!(&response, "Content-Type", "image/png");
+            assert_header!(&response, "Content-Length", "60629");
         });
     });
 
--- tests/test_helper.rs
+++ tests/test_helper.rs
@@ -1,17 +1,13 @@
-extern crate difference;
+pub extern crate difference;
 extern crate reqwest;
 
 use std::process::{Command, Stdio};
 use std::ffi::OsStr;
 use std::fmt::Display;
-use std::io::Read;
 use std::path::Path;
 use std::fs;
 use std::{thread, time};
-use std::fs::File;
-use self::difference::{diff, print_diff};
 pub use self::reqwest::StatusCode;
-use self::reqwest::Response;
 
 const BASE_URL: &str = "http://localhost:4000";
 static SLEEP_TIME: time::Duration = time::Duration::from_millis(500);
@@ -27,6 +23,13 @@ pub fn create_basic_project<P: AsRef<OsStr>>(project_name: P) {
         .arg(project_name)
         .status()
         .expect("Creating project failed.");
+}
+
+pub fn remove_test_project<P: AsRef<OsStr>>(project_name: P) {
+    let path = Path::new("./tests/test_projects");
+    let project_name = Path::new(&project_name).file_name().expect("No project_name specified");
+    let project_path = path.join(project_name);
+    fs::remove_dir_all(project_path).expect("Removing project failed");
 }
 
 pub fn start_project<P, F>(project_name: P, closure: F)
@@ -59,44 +62,54 @@ pub fn get_request<U, F>(url_path: U, closure: F)
     closure(response);
 }
 
-pub fn assert_header<H: AsRef<str>>(response: &Response, header_name: H, header_value: H) {
-    assert!(response.headers().get_raw(header_name.as_ref()).unwrap() == header_value.as_ref());
+#[macro_export]
+macro_rules! assert_header {
+    ($response:expr, $header_name:expr, $header_value:expr) => ({
+        assert!($response.headers().get_raw($header_name).unwrap() == $header_value);
+    })
 }
 
-pub fn assert_status(response: &Response, status: StatusCode) {
-    assert!(response.status() == status);
+#[macro_export]
+macro_rules! assert_status {
+    ($response:expr, $status:expr) => ({
+            assert!($response.status() == $status)
+        })
 }
 
-pub fn remove_test_project<P: AsRef<OsStr>>(project_name: P) {
-    let path = Path::new("./tests/test_projects");
-    let project_name = Path::new(&project_name).file_name().expect("No project_name specified");
-    let project_path = path.join(project_name);
-    fs::remove_dir_all(project_path).expect("Removing project failed");
+#[macro_export]
+macro_rules! assert_file_exists {
+    ($file_path:expr) => ({
+        use std::path::Path;
+        let path = Path::new("./tests/test_projects").join(Path::new(&$file_path));
+        assert!(path.exists())
+    })
 }
 
-pub fn assert_file_exists<P: AsRef<OsStr>>(file_path: P) {
-    let path = Path::new("./tests/test_projects").join(Path::new(&file_path));
-    assert!(path.exists())
-}
+#[macro_export]
+macro_rules! assert_file_contents {
+    ($file_path:expr) => ({
+        use std::io::Read;
+        use std::fs::File;
+        use std::path::Path;
+        use test_helper::difference::{diff, print_diff};
+        let test_file_path = Path::new("./tests/test_projects").join(Path::new(&$file_path));
+        let mut file_contents = String::new();
+        File::open(test_file_path)
+            .expect("Error opening file")
+            .read_to_string(&mut file_contents)
+            .expect("Error reading from file");
 
-pub fn assert_file_contents<P: AsRef<OsStr>>(file_path: P) {
-    let test_file_path = Path::new("./tests/test_projects").join(Path::new(&file_path));
-    let mut file_contents = String::new();
-    File::open(test_file_path)
-        .expect("Error opening file")
-        .read_to_string(&mut file_contents)
-        .expect("Error reading from file");
+        let fixture_path = Path::new("./tests/fixtures").join(Path::new(&$file_path));
+        let mut fixture_contents = String::new();
+        File::open(fixture_path)
+            .expect("Error opening file")
+            .read_to_string(&mut fixture_contents)
+            .expect("Error reading from file");
 
-    let fixture_path = Path::new("./tests/fixtures").join(Path::new(&file_path));
-    let mut fixture_contents = String::new();
-    File::open(fixture_path)
-        .expect("Error opening file")
-        .read_to_string(&mut fixture_contents)
-        .expect("Error reading from file");
-
-    let diff_res = diff(&fixture_contents, &file_contents, "").0;
-    if diff_res != 0 {
-        print_diff(&fixture_contents, &file_contents, "");
-    }
-    assert!(diff_res == 0);
+        let diff_res = diff(&fixture_contents, &file_contents, "").0;
+        if diff_res != 0 {
+            print_diff(&fixture_contents, &file_contents, "");
+        }
+        assert!(diff_res == 0);
+    })
 }
